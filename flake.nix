3b73701ccbd40ea42c1f688d9f2e00175adf24c3
Benedikt Peetz <benedikt.peetz@b-peetz.de>
2023-07-31 22:42:39 +0200

    Build(flake): Add handout to the default output

--- flake.nix
+++ flake.nix
@@ -23,48 +23,58 @@
       pkgs = nixpkgs.legacyPackages.${system};
 
       texlive = pkgs.texlive.combined.scheme-full;
+
+      all = pkgs.runCommand "all" {} ''
+        mkdir $out;
+        cp "${handout}"/main.pdf $out/handout.pdf;
+        cp "${presentation}/main.pdf" $out/presentation.pdf;
+      '';
+
+      handout = pkgs.stdenv.mkDerivation {
+        pname = "latex-handout";
+        version = "1.0";
+        src = ./handout;
+
+        nativeBuildInputs = [
+          texlive
+        ];
+
+        buildPhase = ''
+          latexmk -file-line-error -pdf main.tex
+        '';
+
+        installPhase = ''
+          install -D main.pdf $out/main.pdf;
+        '';
+      };
+
+      presentation = pkgs.stdenv.mkDerivation {
+        pname = "latex-presentation";
+        version = "1.0";
+        src = ./presentation;
+
+        nativeBuildInputs = [
+          texlive
+        ];
+
+        buildPhase = ''
+          latexmk -file-line-error -xelatex main.tex
+        '';
+
+        installPhase = ''
+          install -D main.pdf $out/main.pdf;
+        '';
+        FONTCONFIG_FILE = pkgs.makeFontsConf {
+          fontDirectories = [
+            "${pkgs.fira-mono}/share/fonts/opentype"
+            "${pkgs.fira}/share/fonts/opentype"
+          ];
+        };
+      };
     in {
       packages = {
-        handout = pkgs.stdenv.mkDerivation {
-          pname = "latex-handout";
-          version = "1.0";
-          src = ./handout;
-
-          nativeBuildInputs = [
-            texlive
-          ];
-
-          buildPhase = ''
-            latexmk -file-line-error -pdf main.tex
-          '';
-
-          installPhase = ''
-            install -D main.pdf $out/main.pdf;
-          '';
-        };
-        default = pkgs.stdenv.mkDerivation {
-          pname = "latex-presentation";
-          version = "1.0";
-          src = ./presentation;
-
-          nativeBuildInputs = [
-            texlive
-          ];
-
-          buildPhase = ''
-            latexmk -file-line-error -xelatex main.tex
-          '';
-
-          installPhase = ''
-            install -D main.pdf $out/main.pdf;
-          '';
-          FONTCONFIG_FILE = pkgs.makeFontsConf {
-            fontDirectories = [
-              "${pkgs.fira-mono}/share/fonts/opentype"
-              "${pkgs.fira}/share/fonts/opentype"
-            ];
-          };
-        };
+        default = all;
+        inherit presentation handout all;
       };
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
